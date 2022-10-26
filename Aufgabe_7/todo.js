c776550f36f0c53baa9df5548f3da4bb13659746
Sven <sven_lai@sluz.ch>
2022-10-26 19:16:00 +0200

    Aufgabe 8: Erledigte ToDos mit Button Entferbar.

--- Aufgabe_7/todo.js
+++ Aufgabe_7/todo.js
@@ -1,58 +1,63 @@
 export class ToDo extends EventTarget {
-  #titel = '';
-  #erledigt = '';
+    #titel = '';
+    #erledigt = '';
 
-  constructor(titel, erledigt) {
-    super();
-    this.#titel = titel;
-    this.#erledigt = erledigt;
-  }
-
-  get titel() {
-    return this.#titel;
-  }
-
-  set titel(titel) {
-    this.#titel = titel;
-  }
-
-  get erledigt() {
-    return this.#erledigt;
-  }
-
-  set erledigt(erledigt) {
-    this.#erledigt = erledigt;
-  }
-
-  element() {
-    const listElement = document.createElement('li');
-    const divElement = document.createElement('div');
-    const checkboxElement = document.createElement('input');
-    const spanElement = document.createElement('span');
-    const buttonElement = document.createElement('button');
-
-    listElement.appendChild(divElement);
-
-    divElement.appendChild(checkboxElement);
-    divElement.appendChild(spanElement);
-    divElement.appendChild(buttonElement);
-
-    checkboxElement.setAttribute('type', 'checkbox');
-
-    buttonElement.className = 'loeschen';
-
-    spanElement.innerText = this.#titel;
-    buttonElement.innerText = 'Löschen';
-
-    if (this.#erledigt) {
-      checkboxElement.setAttribute('checked', 'checked');
-      divElement.className = 'erledigt';
+    constructor(titel, erledigt) {
+        super();
+        this.#titel = titel;
+        this.#erledigt = erledigt;
     }
 
-    buttonElement.addEventListener('click', () => {
-      this.dispatchEvent(new Event('loeschen'));
-    });
+    get titel() {
+        return this.#titel;
+    }
 
-    return listElement;
-  }
+    set titel(titel) {
+        this.#titel = titel;
+    }
+
+    get erledigt() {
+        return this.#erledigt;
+    }
+
+    set erledigt(erledigt) {
+        this.#erledigt = erledigt;
+    }
+
+    element() {
+        const listElement = document.createElement('li');
+        const divElement = document.createElement('div');
+        const checkboxElement = document.createElement('input');
+        const spanElement = document.createElement('span');
+        const buttonElement = document.createElement('button');
+
+        listElement.appendChild(divElement);
+
+        divElement.appendChild(checkboxElement);
+        divElement.appendChild(spanElement);
+        divElement.appendChild(buttonElement);
+
+        checkboxElement.setAttribute('type', 'checkbox');
+
+        checkboxElement.addEventListener('change', (e) => {
+            this.#erledigt = e.currentTarget.checked;
+        })
+
+        buttonElement.className = 'loeschen';
+
+        spanElement.innerText = this.#titel;
+        buttonElement.innerText = 'Löschen';
+
+        if (this.#erledigt) {
+            checkboxElement.setAttribute('checked', 'checked');
+            divElement.className = 'erledigt';
+        }
+
+        buttonElement.addEventListener('click', () => {
+            this.dispatchEvent(new Event('loeschen'));
+        });
+
+
+        return listElement;
+    }
 }
